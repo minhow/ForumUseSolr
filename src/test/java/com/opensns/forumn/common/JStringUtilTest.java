d06dbf15adf45762eac244b5e717d3c4197fdef6
opensns853 <opensns853@10.40.242.151>
2016-01-28 11:15:28 +0900

    특수문자제거 수정 테스트

--- src/test/java/com/opensns/forumn/common/JStringUtilTest.java
+++ src/test/java/com/opensns/forumn/common/JStringUtilTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class JStringUtilTest extends TestCase{
 	public void testRemoveSpecialLetter(){
-		String str=JStringUtil.removeSpecialLetter("가나다라 마바사 :=89");
+		String str=JStringUtil.removeSpecialLetter("가나다라 마바사 :=$!@&$^(@$@$^*)(89");
 		assertEquals("가나다라 마바사 89", str);
 	}
 }
